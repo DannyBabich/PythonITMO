b51e896ecc5a671709ef8b5b1486ff9567347959
DannyBabich <danny.babich@gmail.com>
2024-07-10 11:12:15 +0200

    new versions

--- time_skeleton.py
+++ time_skeleton.py
@@ -99,3 +99,55 @@ if __name__ == '__main__':
     assert min < 60, f'Введено число превышающее количество минут в часах: {min}'
     assert hr < 60, f'Введено число превышающее количество секунд в минуте: {sec}'
 
+
+'''Четвертый вариант кода позволяет к текущему времени добавить произвольное колличество часов, минут, секунд
+и возвращает время, которое настанет через указанный промежуток времени'''
+
+import time
+
+class Time(object):
+    def __init__(self, hr, min, sec):
+        self.hr = int(hr)
+        self.min = int(min)
+        self.sec = int(sec)
+
+    def convert(self):
+        self.new_hr = self.hr
+        self.new_min = self.min
+        self.new_sec = self.sec
+        while self.new_hr > 24 or self.new_min > 59 or self.new_sec > 59:
+            while self.new_hr > 24:
+                self.new_hr -= 24
+            while self.new_min > 59:
+                self.new_min -= 60
+                self.new_hr += 1
+            while self.new_sec > 59:
+                self.new_sec -= 60
+                self.new_min += 1
+
+    def output(self):
+        if self.hr > 24 or self.min > 59 or self.sec > 59:
+            self.convert()
+            return [self.new_hr, self.new_min, self.new_sec]
+        else:
+            return [self.hr, self.min, self.sec]
+
+def new_time(time_1, time_2):
+    new_hr = time_1[0] + time_2[0]
+    new_min = time_1[1] + time_2[1]
+    new_sec = time_1[2] + time_2[2]
+    while new_hr > 24 and new_min > 59 and new_sec > 59:
+        while new_hr > 24:
+            new_hr -= 24
+        while new_min > 59:
+            new_min -= 60
+            new_hr += 1
+        while new_sec > 59:
+            new_sec -= 60
+            new_min += 1
+    print (f'{new_hr}:{new_min}:{new_sec}')
+
+if __name__ == '__main__':
+    now = Time(time.strftime('%H'),  time.strftime('%M'), time.strftime('%S'))
+    add_time = Time(50,40,80)
+    new_time(now.output(), add_time.output())
